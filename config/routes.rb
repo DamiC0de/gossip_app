614b2ede3ef5c0c449c4db4318ccba72184cde5e
DamiC0de <dreymidot@gmail.com>
2023-05-14 11:30:33 +0200

    edxit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   get 'home/home'
   get 'static_pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   root 'home#home' # Route pour la page d'accueil
+
+  # Pages statiques
   get 'contact', to: 'home#contact' # Route pour la page de contact
   get 'team', to: 'home#team' # Route pour la page de l'équipe
   get 'register', to: 'home#register' # Route pour la page d'inscription
